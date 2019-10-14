c783b96256b47ba71203ac9aa2081fc67224c186
juzhiyuan <jjzhiyuan@gmail.com>
2019-10-14 04:44:08 +0000

    9b8f8296: clear history build

---
+++ precache-manifest.cf0f6640d9eb134acfe31d1607f611c1.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "aeee7cede3137239d2ed",
+    "url": "/apisix/dashboard/css/chunk-82b74634.2f3ac95f.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/apisix/dashboard/robots.txt"
+  },
+  {
+    "revision": "aeee7cede3137239d2ed",
+    "url": "/apisix/dashboard/js/chunk-82b74634.61c96d11.js"
+  },
+  {
+    "revision": "d20cc52015ed78566222",
+    "url": "/apisix/dashboard/css/404.00c96426.css"
+  },
+  {
+    "revision": "0d25527cca2683ea16cb",
+    "url": "/apisix/dashboard/css/app.38c24f68.css"
+  },
+  {
+    "revision": "0d25527cca2683ea16cb",
+    "url": "/apisix/dashboard/js/app.1ffe499e.js"
+  },
+  {
+    "revision": "4086b88645bfa62d3d64",
+    "url": "/apisix/dashboard/js/auth-redirect.ea1dc88f.js"
+  },
+  {
+    "revision": "1f9c111c7b922e9fae39",
+    "url": "/apisix/dashboard/css/chunk-2d45a3ee.d7e0e035.css"
+  },
+  {
+    "revision": "1f9c111c7b922e9fae39",
+    "url": "/apisix/dashboard/js/chunk-2d45a3ee.43896746.js"
+  },
+  {
+    "revision": "d42f59af1a0b0bc3a15b",
+    "url": "/apisix/dashboard/js/chunk-2fccb682.e67773d9.js"
+  },
+  {
+    "revision": "f7b898c4521f9dc546e4",
+    "url": "/apisix/dashboard/css/chunk-35f1b43d.9b00da3b.css"
+  },
+  {
+    "revision": "f7b898c4521f9dc546e4",
+    "url": "/apisix/dashboard/js/chunk-35f1b43d.a94aa3f1.js"
+  },
+  {
+    "revision": "2dd849e0d5f54dce5007",
+    "url": "/apisix/dashboard/css/chunk-424f432e.200293c4.css"
+  },
+  {
+    "revision": "2dd849e0d5f54dce5007",
+    "url": "/apisix/dashboard/js/chunk-424f432e.e1bac221.js"
+  },
+  {
+    "revision": "b499f5edb4c68e4807bb",
+    "url": "/apisix/dashboard/css/chunk-48d71f84.cea5e2a7.css"
+  },
+  {
+    "revision": "b499f5edb4c68e4807bb",
+    "url": "/apisix/dashboard/js/chunk-48d71f84.fef10ebf.js"
+  },
+  {
+    "revision": "e8938fe48f00100f94a5",
+    "url": "/apisix/dashboard/css/chunk-4a5cca17.200293c4.css"
+  },
+  {
+    "revision": "e8938fe48f00100f94a5",
+    "url": "/apisix/dashboard/js/chunk-4a5cca17.e3a6a329.js"
+  },
+  {
+    "revision": "db8c624214874899cb65",
+    "url": "/apisix/dashboard/css/chunk-4cf90b49.8db8c2e3.css"
+  },
+  {
+    "revision": "db8c624214874899cb65",
+    "url": "/apisix/dashboard/js/chunk-4cf90b49.f7f0b8a7.js"
+  },
+  {
+    "revision": "5121566d2959509770fa",
+    "url": "/apisix/dashboard/css/chunk-6b2d316b.32f5687b.css"
+  },
+  {
+    "revision": "5121566d2959509770fa",
+    "url": "/apisix/dashboard/js/chunk-6b2d316b.206e29c9.js"
+  },
+  {
+    "revision": "37fc619e228602e63539",
+    "url": "/apisix/dashboard/js/401.dedc9950.js"
+  },
+  {
+    "revision": "d20cc52015ed78566222",
+    "url": "/apisix/dashboard/js/404.4e5e2009.js"
+  },
+  {
+    "revision": "9466a6bd9deda1d83091",
+    "url": "/apisix/dashboard/css/chunk-91ff715a.200293c4.css"
+  },
+  {
+    "revision": "9466a6bd9deda1d83091",
+    "url": "/apisix/dashboard/js/chunk-91ff715a.5a600e83.js"
+  },
+  {
+    "revision": "a4627451aa77df18302a",
+    "url": "/apisix/dashboard/js/chunk-elementUI.77a60e96.js"
+  },
+  {
+    "revision": "df755ebf5d41a207102c",
+    "url": "/apisix/dashboard/css/chunk-libs.ee57d822.css"
+  },
+  {
+    "revision": "df755ebf5d41a207102c",
+    "url": "/apisix/dashboard/js/chunk-libs.e0ff45d6.js"
+  },
+  {
+    "revision": "54aaa54ac993a5751d3d",
+    "url": "/apisix/dashboard/css/complex-table.cea5e2a7.css"
+  },
+  {
+    "revision": "54aaa54ac993a5751d3d",
+    "url": "/apisix/dashboard/js/complex-table.68b038c6.js"
+  },
+  {
+    "revision": "ca84e9f8fb6ca9680ff8",
+    "url": "/apisix/dashboard/css/login.163d5854.css"
+  },
+  {
+    "revision": "ca84e9f8fb6ca9680ff8",
+    "url": "/apisix/dashboard/js/login.ea89c9fa.js"
+  },
+  {
+    "revision": "2084bb35d26eb482ce5d",
+    "url": "/apisix/dashboard/css/profile.ddf910bb.css"
+  },
+  {
+    "revision": "2084bb35d26eb482ce5d",
+    "url": "/apisix/dashboard/js/profile.1703adf6.js"
+  },
+  {
+    "revision": "a5436c1432d1006a90ac",
+    "url": "/apisix/dashboard/js/redirect.69d13fca.js"
+  },
+  {
+    "revision": "6c4e9a0bd40861d36c0d",
+    "url": "/apisix/dashboard/js/runtime.145fb838.js"
+  },
+  {
+    "revision": "0f4bc32b0f52f7cfb7d19305a6517724",
+    "url": "/apisix/dashboard/img/404-cloud.0f4bc32b.png"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/apisix/dashboard/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/apisix/dashboard/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "a57b6f31fa77c50f14d756711dea4158",
+    "url": "/apisix/dashboard/img/404.a57b6f31.png"
+  },
+  {
+    "revision": "089007e721e1f22809c0313b670a36f1",
+    "url": "/apisix/dashboard/img/401.089007e7.gif"
+  },
+  {
+    "revision": "79c87389c8c620ce3fe1fd40d1a1f7ca",
+    "url": "/apisix/dashboard/index.html"
+  },
+  {
+    "revision": "37fc619e228602e63539",
+    "url": "/apisix/dashboard/css/401.289c91ea.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,19 @@
+importScripts("/apisix/dashboard/precache-manifest.cf0f6640d9eb134acfe31d1607f611c1.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+// This is the code piece that GenerateSW mode can't provide for us.
+// This code listens for the user's confirmation to update the app.
+self.addEventListener('message', (e) => {
+  if (e.data) {
+    if (e.data === 'skipWaiting') {
+      self.skipWaiting()
+    }
+  }
+})
+
+workbox.clientsClaim()
+
+// The precaching code provided by Workbox. You don't need to change this part.
+self.__precacheManifest = [].concat(self.__precacheManifest || [])
+workbox.precaching.suppressWarnings()
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
+
